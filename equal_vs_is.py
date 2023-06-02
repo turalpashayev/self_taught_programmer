12eee0e231dc677b7fe875eea8c3d2467585aad9
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-02 14:39:16 -0700

    equals_vs_is file is addded

---
+++ equal_vs_is.py
@@ -0,0 +1,12 @@
+# What is difference between '==' and 'is'?
+# '==' checks for equality, 'is' checks for identity
+# For example:
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a == b)  # True
+print(a is b)  # False
+
+# check location of a and b
+print(id(a))
+print(id(b))
