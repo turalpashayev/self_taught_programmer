8f8efe5e2fda93267d98c4e8f8a43effb1400b4b
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-03 22:14:08 +0000

    created a new python file hangman

---
+++ hangman.py
@@ -0,0 +1,11 @@
+'''
+It is text-based game called Hangman
+Player 1 picks a secret word and draws line to represent each word
+Player 2 tries to guess the word at a time.
+If Player 2 guesses word correctly Player 1 replaces undescore with correct the letter
+Otherwise Player 1 will draw a hang man.
+If Player 2 guesses the word before hangman is complete then wins otherwise lose.
+'''
+
+def hangman(word):
+    pass
