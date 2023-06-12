8186a75dcec6380134bf05137ff711bc9828950e
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-12 06:13:32 -0700

    string_manupulation file is added

---
+++ string_manupulation.py
@@ -0,0 +1,20 @@
+# String manupulation
+
+# Join method lets you add new character between every character in a string
+abc = 'abc'
+new_abc = '+'.join(abc)
+# print(new_abc)
+
+
+l_abc = ['a','b','c']
+new_l = ''.join(l_abc)
+print(new_l)
+
+
+
+# Split method in a string object
+email = 'tural_pashayev@yahoo.com'
+a = email.split('@')
+# print(a[0])
+
+
