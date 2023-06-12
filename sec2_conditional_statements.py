05aae94d017784f7a04f28d44e8e196259994728
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-12 09:04:29 -0700

    sec2 items are added

---
+++ sec2_conditional_statements.py
@@ -0,0 +1,8 @@
+# Conditional statements
+
+x=4
+if x%2 == 0:
+    if x > 1 and x<=5:
+        print('x is even and between 1 and 5 included')
+else:
+    print('x is odd')
---
+++ sec2_simple_compound_statments.py
@@ -0,0 +1,21 @@
+# Simple and Compound statments
+
+# Simple statment
+print('Hello World')
+
+# # Compound statements
+home = 'United States'
+
+if home == 'United States':
+    print('Welcome to United States')
+else:
+    print('Hello World')
+
+
+# Usage of input function
+
+is_home = input('Enter your home: ')
+if is_home == 'United States':
+    print('Welcome to United States')
+else:
+    print('Hello World')
