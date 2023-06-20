3e7cd0b69eb026536619bb50a3bf167eed1ddbfe
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-20 10:08:52 -0700

    sec3 more libraries are added

---
+++ sec3_docstrings.py
@@ -0,0 +1,10 @@
+# docstrings are used to document functions, classes, and modules
+ 
+def add(x, y):
+    """
+    Returns x + y.
+    param x: int.
+    param y: int.
+    return: int sum of x and y.    
+    """
+    return x + y
---
+++ sec3_exception_handling.py
@@ -0,0 +1,30 @@
+# Exception Handling
+
+# code block without exception handling
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+a = int(a)
+b = int(b)
+print(a / b)
+
+# code block with exception handling
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+a = int(a)
+b = int(b)
+
+try:
+    print(a / b)
+except ZeroDivisionError:
+    print("b cannot be zero")
+
+# more code block with exception handling
+a = input("Enter a number: ")  
+b = input("Enter another number: ")
+a = int(a)
+b = int(b)
+
+try:
+    print(a / b)
+except (ZeroDivisionError, ValueError):
+    print("Invalid input(s)")
