188bde261b47eab946dbb3c45e89cc6717db5453
Tural Pashayev <35586555+turalpashayev@users.noreply.github.com>
2023-06-25 06:49:31 -0700

    Created sec4 to cover containers: lists, tuples, dctionaries

---
+++ sec4_list.py
@@ -0,0 +1,11 @@
+# We can create lists with two different ways:
+fruit = list()
+fruit = []
+
+# Creating lists with items already in it
+fruits = ['apple', 'orange', 'banana']
+
+# Using append method to add items to the list
+fruits.append('pear')
+fruits.append('kiwi')
+print(fruits)
---
+++ sec4_method.py
@@ -0,0 +1,8 @@
+# We call merhod on an object, and it will return a value
+# Upper method will return a string with all the characters in uppercase
+hello1 =  'Hello'.upper()
+print(hello1)
+
+# Replace method will replace the first argument with the second argument
+hello2 = 'Hello'.replace('o', '@')
+print(hello2)
