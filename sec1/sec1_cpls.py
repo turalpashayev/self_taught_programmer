b3ee714c9bf15194a056c4cbf086cbdc0196d262
turalpashayev <tural.pashayev24@gmail.com>
2023-12-06 08:15:40 -0800

    Added sec1_clps file

---
+++ sec1/sec1_cpls.py
@@ -0,0 +1,16 @@
+# Comments, Printing, Lines, Spacing
+
+# this is a comment
+"""
+This is a long
+long
+long
+comment
+"""
+
+print("Hello, World")
+
+# line 1
+# line 2
+for i in range(100):
+    print("Hello world")
