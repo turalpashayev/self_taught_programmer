1d042783e1ec066bb056811d643f77993998f75c
Tural Pashayev <tural_pasha@turals-mbp.local>
2024-01-13 21:54:12 +0300

    created hangman game

--- projects/hangman.py
+++ projects/hangman.py
@@ -8,4 +8,41 @@ If Player 2 guesses the word before hangman is complete then wins otherwise lose
 '''
 
 def hangman(word):
-    pass
+    wrong = 0
+    stages = ["",
+              "_________       ",
+              "|         ",
+              "|    |    ",
+              "|    O    ",
+              "|   /|\\   ",
+              "|   / \\   ",
+              "|         ",
+    ]
+    rletters = list(word)
+    board = ["__"]*len(word)
+    win = False
+    print("Welcome to Hangman game")
+
+    while wrong < len(stages)-1:
+        print("\n")
+        message = "Guess a letter\n"
+        char = input(message)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+            print((" ".join(board)))
+            e = wrong + 1
+            print("\n".join(stages[0:e]))
+        if "__" not in board:
+            print("You win!")
+            print("".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong]))
+        print("\n You lose! Word was {} \n".format(word))
+
+hangman('cat')
